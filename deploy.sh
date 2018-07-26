e73fb3706c596f6db84fb98cf9655cfa5897056c
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-07-26 10:52:52 +0200

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -28,10 +28,10 @@ go get github.com/hyperledger/fabric/core/chaincode/shim
 go get github.com/rs/xid
 go get github.com/hyperledger/fabric/protos/peer
 go build
-CORE_CHAINCODE_LOGLEVEL=debug CORE_PEER_ADDRESS=$HOST:10051 CORE_CHAINCODE_ID_NAME=dcot-chaincode:$1 ./dcot-chaincode
+#CORE_CHAINCODE_LOGLEVEL=debug CORE_PEER_ADDRESS=$HOST:10051 CORE_CHAINCODE_ID_NAME=dcot-chaincode:$1 ./dcot-chaincode
 cd .. && cp -fR dcot-chaincode $FABRIC_DIR/chaincode/go
 echo "Install chaincode"
 docker exec -it cli peer chaincode install -n dcot-chaincode -v $1 -p github.com/hyperledger/fabric/examples/chaincode/go/dcot-chaincode
 sleep 5
 echo "Instantiate chaincode"
-docker exec -it cli peer chaincode $OPERATION -n dcot-chaincode -c '{"Args":[""]}' -C $CHANNEL -v $1
+docker exec -it cli bash peer chaincode $OPERATION -n dcot-chaincode -c '{"Args":[""]}' -C $CHANNEL -v $1
